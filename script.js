da423268ffb22f610edc14f3f1f6b0a69fa38156
Fernetazo <angelofdarkness88@gmail.com>
2021-08-13 16:54:03 -0300

    Mathematical operations

---
+++ script.js
@@ -0,0 +1,109 @@
+let num1 = null;
+let num2 = null;
+let operator = false;
+let display = document.getElementById("display");
+let result = null;
+let inputtedOperation = null;
+
+function buttonClicked(e) {
+    userInput(e);
+}
+
+function buttonPressed(e) {
+    userInput(e);
+}
+
+let add = (a, b) => parseFloat(a) + parseFloat(b);
+
+let sub = (a, b) => parseFloat(a) - parseFloat(b);
+
+let multi = (a, b) => parseFloat(a) * parseFloat(b);
+
+let divi = (a, b) => parseFloat(a) / parseFloat(b);
+
+let pow = (a, b) => Math.pow(parseFloat(a), parseFloat(b));
+
+let root = (a, b) => Math.pow(parseFloat(b), 1/parseFloat(a));
+
+let percent = (a, b) => ((parseFloat(b) / 100) * parseFloat(a));
+
+function userInput(e) {
+    let input = (e.keyCode || e.target.textContent);
+    let type = e.target.classList[0]
+
+    if (type == "number") {
+        if (operator == false) {
+            if (num1 == null) {
+                num1 = input;
+            } else if (num1 != null) {
+                num1 = num1 + input;
+            }
+            display.textContent = num1;
+        } else {
+            if (num2 == null) {
+                num2 = input;
+            } else if (num2 != null) {
+                num2 = num2 + input;
+            }
+            display.textContent = num2;
+        }
+    }
+    
+    if (type == "operator") {
+        if (input != "=") {
+            inputtedOperation = input;
+            if (num1 && num2) {
+                if (input == "+") {
+                    result = add(num1, num2);
+                } else if (input == "-") {
+                    result = sub(num1, num2);
+                } else if (input == "*") {
+                    result = multi(num1, num2);
+                } else if (input == "/") {
+                    result = divi(num1, num2);
+                } else if (input == "xⁿ") {
+                    result = pow(num1, num2);
+                } else if (input == "ⁿ√") {
+                    result = root(num1, num2);
+                } else if (input == "%") {
+                    result = percent(num1, num2);
+                }
+                display.textContent = result;
+                num1 = result;
+                num2 = null;
+                operator = true;
+            } else if (result && !num1) {
+                num1 = result;
+                display.textContent = input;
+                operator = true;
+            } else {
+                display.textContent = input;
+                operator = true;
+            }
+        } else if (input == "=") {
+            if (inputtedOperation == "+") {
+                result = add(num1, num2);
+            } else if (inputtedOperation == "-") {
+                result = sub(num1, num2);
+            } else if (inputtedOperation == "*") {
+                result = multi(num1, num2);
+            } else if (inputtedOperation == "/") {
+                result = divi(num1, num2);
+            } else if (inputtedOperation == "xⁿ") {
+                result = pow(num1, num2);
+            } else if (inputtedOperation == "ⁿ√") {
+                result = root(num1, num2);
+            } else if (inputtedOperation == "%") {
+                result = percent(num1, num2);
+            }
+            display.textContent = result;
+            num1 = null;
+            num2 = null;
+            operator = false;
+        }
+    }
+}
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => button.addEventListener('click', buttonClicked))
+window.addEventListener('keyup', buttonPressed);
